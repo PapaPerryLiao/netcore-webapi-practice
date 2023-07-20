546fdf7515c7171f1c2ee9303b1cf66826a47ff5
PapaPerryLiao <perry830815@gmail.com>
2023-07-21 01:16:50 +0800

    feat: product controller

---
+++ netcore-webapi-practice/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using netcore_webapi_practice.Models;
+using netcore_webapi_practice.Service;
+
+namespace netcore_webapi_practice.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> Get()
+        {
+            var products = await _productService.GetProducts();
+            return Ok(products);
+        }
+    }
+}
