a44c27797f85cd81d11f4086c8bfd9a9c09506b5
PapaPerryLiao <perry830815@gmail.com>
2023-07-21 01:16:34 +0800

    feat: product service

---
+++ netcore-webapi-practice/Service/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using netcore_webapi_practice.Models;
+
+namespace netcore_webapi_practice.Service
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<Product>> GetProducts();
+    }
+}
---
+++ netcore-webapi-practice/Service/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using netcore_webapi_practice.Models;
+using netcore_webapi_practice.Repository;
+
+namespace netcore_webapi_practice.Service
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<Product>> GetProducts()
+        {
+            return await _productRepository.GetProducts();
+        }
+    }
+}
