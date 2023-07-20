a18524e6bb1d46bfadbadb8156c665bf228ca4c7
PapaPerryLiao <perry830815@gmail.com>
2023-07-20 17:12:28 +0800

    feat: Northwind DbContext

---
+++ netcore-webapi-practice/Contexts/NorthwindDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using netcore_webapi_practice.Models;
+
+namespace netcore_webapi_practice.Contexts
+{
+    public class NorthwindDbContext: DbContext
+    {
+        public NorthwindDbContext(DbContextOptions<NorthwindDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
