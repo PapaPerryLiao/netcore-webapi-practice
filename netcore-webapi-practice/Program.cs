e805a00543a7cbd338dc14bfe611b3c9a0a2e08a
PapaPerryLiao <perry830815@gmail.com>
2023-07-21 01:13:42 +0800

    fix: use Program.cs instead of Startup.cs in .Net6

--- netcore-webapi-practice/Program.cs
+++ netcore-webapi-practice/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using netcore_webapi_practice.Contexts;
+using netcore_webapi_practice.Repository;
+using netcore_webapi_practice.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+string connectionString = builder.Configuration.GetConnectionString("NorthwindDbConnection");
+
+builder.Services.AddDbContext<NorthwindDbContext>(option => option.UseSqlServer(connectionString));
+builder.Services.AddTransient<IProductService, ProductService>();
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
