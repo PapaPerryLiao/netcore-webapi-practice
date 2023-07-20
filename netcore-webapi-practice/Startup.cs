15cfd83fb5dbf82694a2c10ba44eb5006443b912
PapaPerryLiao <perry830815@gmail.com>
2023-07-20 18:18:09 +0800

    feat: add db connection

---
+++ netcore-webapi-practice/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using netcore_webapi_practice.Contexts;
+
+namespace netcore_webapi_practice
+{
+    public class Startup
+    {
+
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            string connectionString = Configuration.GetConnectionString("NorthwindDbConnection");
+
+            services.AddDbContext<NorthwindDbContext>(option => option.UseSqlServer(connectionString));
+
+        }
+
+    }
+}
